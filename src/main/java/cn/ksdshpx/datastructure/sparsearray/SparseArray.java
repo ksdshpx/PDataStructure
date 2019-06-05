4dbcab4704c5dd9129147d21c88a872d4061178f
ksdshpx <ksdshpx2018@aliyun.com>
2019-06-05 15:45:39 +0800

    稀疏数组的应用！

--- src/main/java/cn/ksdshpx/datastructure/sparsearray/SparseArray.java
+++ src/main/java/cn/ksdshpx/datastructure/sparsearray/SparseArray.java
@@ -5,7 +5,78 @@ package cn.ksdshpx.datastructure.sparsearray;
  * Create by peng.xing
  * Date: 2019/6/5
  * Time: 14:53
- * Description:
+ * Description:稀疏数组的应用
  */
 public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个原始数组 11*11
+        //默认没有棋子为0,1为黑子,2为蓝子
+        int[][] chessArray = new int[11][11];
+        chessArray[1][2] = 1;
+        chessArray[2][3] = 2;
+        //输出原始数组
+        System.out.println("原始数组为：");
+        for (int[] row : chessArray) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        //将二维数组转换为稀疏数组
+        //1.先遍历二维数组，得到非0数据的个数
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArray[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println("sum=" + sum);
+        //2.创建稀疏数组
+        int[][] sparseArray = new int[sum + 1][3];
+        //3.给稀疏数组赋值
+        sparseArray[0][0] = 11;
+        sparseArray[0][1] = 11;
+        sparseArray[0][2] = sum;
+        //4.遍历二维数组，将非0的值存放到稀疏数组中
+        int count = 0;//用于记录是第几个非0数据
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArray[i][j] != 0) {
+                    count++;
+                    sparseArray[count][0] = i;
+                    sparseArray[count][1] = j;
+                    sparseArray[count][2] = chessArray[i][j];
+                }
+            }
+        }
+        //输出稀疏数组
+        System.out.println();
+        System.out.println("稀疏数组为：");
+        for (int[] row : sparseArray) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        //将稀疏数组恢复成原始数组
+        //1.先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组
+        int[][] chessArray2 = new int[sparseArray[0][0]][sparseArray[0][1]];
+        //2.读取稀疏数组后几行的数据，并赋给原始的二维数组
+        for (int i = 1; i < sparseArray.length; i++) {
+            chessArray2[sparseArray[i][0]][sparseArray[i][1]] = sparseArray[i][2];
+        }
+        //3.输出恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组：");
+        for (int[] row : chessArray2) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+    }
 }
